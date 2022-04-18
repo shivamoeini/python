4f84b6cc80b2482167c071012be0d0afc78212f5
shiva <shivamoeini2000@gmail.com>
2022-04-18 12:48:28 +0430

    cmit7

---
+++ one/7.py
@@ -0,0 +1,9 @@
+number=int(input())
+if number %3==0 and number%7==0:
+    print('bar har do bakhs pazir ast')
+elif number%3 == 0:
+    print('mazrab 3 ast')
+elif number%7 ==0:
+    print ('mazrab 7 ast')
+else:
+    print ('bar hich kodam bakhsh pazir nist')
