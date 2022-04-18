bc3d6085fe32fe0915b4604b09cb835d359750b8
shiva <shivamoeini2000@gmail.com>
2022-04-19 02:42:17 +0430

    cmit4

--- two/4.py
+++
@@ -1,7 +0,0 @@
-n = int(input('tedad afrad ra vared konid:'))
-dic_name = {}
-for i in range(1, n + 1):
-    name = input(f'nam fard {i} ra vared konid:')
-    sen = input(f'sen fard {i} ra vared konid:')
-    dic_name[name] = sen
-print(dic_name)
